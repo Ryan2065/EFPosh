c2f903a92f21c3d5c333322d5b2727e5810c57b4
Ryan Ephgrave <ryan2065@gmail.com>
2020-05-08 14:49:16 -0500

    changed some paths

--- src/buildModule.ps1
+++ src/buildModule.ps1
@@ -6,13 +6,13 @@ dotnet publish --self-contained --configuration release
 
 $Files = ".\EFPosh\bin\Release\net472\publish\*"
 
-if(Test-Path "$PSScriptRoot\Module\bin"){
-    & cmd /c rd "$PSScriptRoot\Module\bin\" /s /q
+if(Test-Path "$PSScriptRoot\Module\EFPosh\bin"){
+    & cmd /c rd "$PSScriptRoot\Module\EFPosh\bin\" /s /q
 }
 
-$null = New-Item "$PSScriptRoot\Module\bin" -ItemType Directory -Force
+$null = New-Item "$PSScriptRoot\Module\EFPosh\bin" -ItemType Directory -Force
 
-Copy-Item -Path $Files -Destination "$PSScriptRoot\Module\bin\" -Force -Recurse
+Copy-Item -Path $Files -Destination "$PSScriptRoot\Module\EFPosh\bin\" -Force -Recurse
 
 Push-Location $Location
 
