5e419b689f79ae568b8c44278dda2d3965e71467
Ryan Ephgrave <Ryan2065@gmail.com>
2022-04-25 18:00:54 -0500

    Current version

--- build.ps1
+++ build.ps1
@@ -3,17 +3,16 @@ $RebuildBinaries = $true
 $ModuleLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "Module", "EFPosh")
 $EFPoshProjLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh", "EFPosh")
 
-
 #region Publish
 
-$BinaryExpConvertProjLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh", "BinaryExpressionConverter")
+$EFPoshEFInteractionsProjLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh", "EFPosh.EFInteractions")
 
 $LastBuildTimeFile = [System.IO.Path]::Combine($($env:temp), "EFPoshLastBuildTime.txt")
 if(Test-Path $LastBuildTimeFile -ErrorAction SilentlyContinue){
   try{
     $LastBuildTime = Get-Content $LastBuildTimeFile | ConvertFrom-JSON
     $FilesToCheck = Get-ChildItem $EFPoshProjLocation -File
-    $FilesToCheck += Get-ChildItem $BinaryExpConvertProjLocation -File
+    $FilesToCheck += Get-ChildItem $EFPoshEFInteractionsProjLocation -File
     $RebuildBinaries = $false
     foreach($file in $FilesToCheck){
       if($LastBuildTime -lt $file.LastWriteTime){
@@ -30,9 +29,12 @@ if($RebuildBinaries){
 
   dotnet restore
   
-  dotnet publish --self-contained false --configuration release --framework net472 --no-restore
-  dotnet publish --self-contained false --configuration release --framework net6.0 --no-restore 
+  #dotnet publish --self-contained false --configuration release --framework netstandard2.0 --no-restore
+  dotnet publish --self-contained true --configuration release --framework net6.0 --no-restore 
   
+  Push-Location $EFPoshEFInteractionsProjLocation
+  dotnet publish --self-contained false --configuration release --framework net6.0
+
   $ModuleDependencyFolder = [System.IO.Path]::Combine($ModuleLocation, "Dependencies")
   
   if(Test-Path $ModuleDependencyFolder){
@@ -46,11 +48,21 @@ if($RebuildBinaries){
   $null = New-Item $Net47Folder -ItemType Directory -Force
   $null = New-Item $Net6Folder -ItemType Directory -Force
   
-  $Net47PublishFolder = [System.IO.Path]::Combine($EFPoshProjLocation, 'bin', 'Release', 'net472', 'publish')
-  $Net6PublishFolder = [System.IO.Path]::Combine($EFPoshProjLocation, 'bin', 'Release', 'net6.0', 'publish')
-  $null = Copy-Item -Path "$Net47PublishFolder\*" -Destination "$Net47Folder\"  -Force -Recurse
-  $null = Copy-Item -Path "$Net6PublishFolder\*" -Destination "$Net6Folder\"  -Force -Recurse
+  Get-ChildItem $ModuleLocation -File | ForEach-Object {
+    if(@('EFPosh.psd1', 'EFPosh.psm1') -notcontains $file.Name){
+      $null = Remove-Item $File.FullName -Force
+    }
+  }
+
+  $Net47PublishFolder = [System.IO.Path]::Combine($EFPoshEFInteractionsProjLocation, 'bin', 'Release', 'net472', 'publish')
+  $Net6PublishFolder = [System.IO.Path]::Combine($EFPoshEFInteractionsProjLocation, 'bin', 'Release', 'net6.0', 'publish')
   
+
+  $null = Copy-Item -Path "$Net47PublishFolder\*" -Destination "$Net47Folder\"  -Force -Recurse
+  $null = Copy-Item -Path "$Net6PublishFolder\*" -Destination "$ModuleLocation\"  -Force -Recurse
+  $null = Copy-Item -Path "$ALCPublishFolder\*" -Destination "$ALCFolder\"  -Force -Recurse
+  
+
   $refFolderPath = [System.IO.Path]::Combine($Net6Folder, 'ref')
   if(Test-Path $refFolderPath){
       Remove-Item $refFolderPath -Force -Recurse
@@ -60,7 +72,8 @@ if($RebuildBinaries){
       'win-x64',
       'win-x86',
       'linux-x64',
-      'osx'
+      'osx',
+      'win'
   )
   $RuntimeFolders = Get-ChildItem ([System.IO.Path]::Combine($Net6Folder, 'runtimes')) -Directory
   foreach($RuntimeFolder in $RuntimeFolders){
