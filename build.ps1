960fdb97f81d0097c72a6ffe62ce749808e03731
Ryan Ephgrave <Ryan2065@gmail.com>
2022-04-25 20:37:40 -0500

    getting assembly load context working

--- build.ps1
+++ build.ps1
@@ -1,18 +1,23 @@
 $RebuildBinaries = $true
 
 $ModuleLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "Module", "EFPosh")
-$EFPoshProjLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh", "EFPosh")
-
+$EFPoshProjLocations = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh")
+$ErrorActionPreference = 'Stop'
 #region Publish
 
-$EFPoshEFInteractionsProjLocation = [System.IO.Path]::Combine($PSScriptRoot, "src", "EFPosh", "EFPosh.EFInteractions")
+$Dependencies = @('EFPosh', 'EFPosh.EFInteractions')
+$Frameworks = @('net6.0', 'netstandard2.0')
+
+$FilesToCheck = @()
+foreach($dep in $Dependencies){
+  $FilesToCheck += Get-ChildItem ([System.IO.Path]::Combine($EFPoshProjLocations, $dep)) -File -Filter '*.cs' -Recurse -Depth 2 -ErrorAction SilentlyContinue
+}
+
 
 $LastBuildTimeFile = [System.IO.Path]::Combine($($env:temp), "EFPoshLastBuildTime.txt")
 if(Test-Path $LastBuildTimeFile -ErrorAction SilentlyContinue){
   try{
     $LastBuildTime = Get-Content $LastBuildTimeFile | ConvertFrom-JSON
-    $FilesToCheck = Get-ChildItem $EFPoshProjLocation -File
-    $FilesToCheck += Get-ChildItem $EFPoshEFInteractionsProjLocation -File
     $RebuildBinaries = $false
     foreach($file in $FilesToCheck){
       if($LastBuildTime -lt $file.LastWriteTime){
@@ -25,45 +30,29 @@ if(Test-Path $LastBuildTimeFile -ErrorAction SilentlyContinue){
 }
 
 if($RebuildBinaries){
-  Push-Location $EFPoshProjLocation
-
-  dotnet restore
-  
-  #dotnet publish --self-contained false --configuration release --framework netstandard2.0 --no-restore
-  dotnet publish --self-contained true --configuration release --framework net6.0 --no-restore 
-  
-  Push-Location $EFPoshEFInteractionsProjLocation
-  dotnet publish --self-contained false --configuration release --framework net6.0
-
   $ModuleDependencyFolder = [System.IO.Path]::Combine($ModuleLocation, "Dependencies")
-  
   if(Test-Path $ModuleDependencyFolder){
-      Remove-Item $ModuleDependencyFolder -Force -Recurse
+    Remove-Item $ModuleDependencyFolder -Force -Recurse
   }
-  
-  $Net47Folder = [System.IO.Path]::Combine($ModuleDependencyFolder, "net472")
-  $Net6Folder = [System.IO.Path]::Combine($ModuleDependencyFolder, "net6.0")
-  
+
   $null = New-Item $ModuleDependencyFolder -ItemType Directory -Force
-  $null = New-Item $Net47Folder -ItemType Directory -Force
-  $null = New-Item $Net6Folder -ItemType Directory -Force
-  
-  Get-ChildItem $ModuleLocation -File | ForEach-Object {
-    if(@('EFPosh.psd1', 'EFPosh.psm1') -notcontains $file.Name){
-      $null = Remove-Item $File.FullName -Force
+
+  foreach($dep in $Dependencies){
+    Push-Location ([System.IO.Path]::Combine($EFPoshProjLocations, $dep))
+    dotnet restore
+    foreach($Framework in $Frameworks){
+      Get-Location
+      
+      dotnet publish --self-contained false --configuration release --framework $Framework --no-restore
+      $copyToFolder = [System.IO.Path]::Combine($ModuleDependencyFolder, $dep, $framework)
+      
+      $null = New-Item $copyToFolder -ItemType Directory -Force
+      $copyFromFolder = [System.IO.Path]::Combine($EFPoshProjLocations, $dep, 'bin', 'Release', $framework, 'publish')
+      $null = Copy-Item -Path "$copyFromFolder\*" -Destination "$copyToFolder\"  -Force -Recurse
+      Pop-Location
     }
   }
-
-  $Net47PublishFolder = [System.IO.Path]::Combine($EFPoshEFInteractionsProjLocation, 'bin', 'Release', 'net472', 'publish')
-  $Net6PublishFolder = [System.IO.Path]::Combine($EFPoshEFInteractionsProjLocation, 'bin', 'Release', 'net6.0', 'publish')
-  
-
-  $null = Copy-Item -Path "$Net47PublishFolder\*" -Destination "$Net47Folder\"  -Force -Recurse
-  $null = Copy-Item -Path "$Net6PublishFolder\*" -Destination "$ModuleLocation\"  -Force -Recurse
-  $null = Copy-Item -Path "$ALCPublishFolder\*" -Destination "$ALCFolder\"  -Force -Recurse
-  
-
-  $refFolderPath = [System.IO.Path]::Combine($Net6Folder, 'ref')
+  <#$refFolderPath = [System.IO.Path]::Combine($Net6Folder, 'ref')
   if(Test-Path $refFolderPath){
       Remove-Item $refFolderPath -Force -Recurse
   }
@@ -80,7 +69,7 @@ if($RebuildBinaries){
       if($RuntimeFoldersToKeep -notcontains $RuntimeFolder.BaseName){
           Remove-Item $RuntimeFolder.FullName -Recurse -Force
       }
-  }
+  }#>
   [DateTime]::Now | ConvertTo-JSON | Out-File $LastBuildTimeFile -Force
 }
 
---
+++ src/EFPosh/EFPosh/AssemblyLoader/EFPoshAssemblyLoadContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+#if NET6_0_OR_GREATER
+using System.Runtime.Loader;
+#endif
+using System.Text;
+
+namespace EFPosh.AssemblyLoader
+{
+#if NET6_0_OR_GREATER
+    public class EFPoshAssemblyLoadContext : AssemblyLoadContext
+    {
+        private readonly List<string> SearchLocations = new List<string>();
+        public EFPoshAssemblyLoadContext()
+        {
+            var assemblyLocation = typeof(EFPoshAssemblyLoadContext).Assembly.Location;
+            // assembly is stored in Dependencies\EFPosh\<Framework> - Need to get to Dependencies\EFPosh.EFPoshInteractions\<Framework>
+            var assemblyLocationParent = System.IO.Directory.GetParent(assemblyLocation);
+            var frameworkName = assemblyLocationParent.Name;
+
+
+
+            var interactionsFolder = Path.Combine(assemblyLocationParent.Parent.FullName, "EFPosh.EFPoshInteractions", frameworkName);
+            
+            SearchLocations.Add(interactionsFolder);
+        }
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            foreach(var searchLocation in SearchLocations)
+            {
+                string assemblyPath = Path.Combine(searchLocation,$"{assemblyName.Name}.dll");
+                if (File.Exists(assemblyPath))
+                {
+                    return LoadFromAssemblyPath(assemblyPath);
+                }
+            }
+            
+            return null;
+        }
+    }
+#endif
+}
